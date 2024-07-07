eb654a9fead5bc54f72150d9298ec0da99ba5748
xxxsen <naivesen@gmail.com>
2024-07-07 14:30:13 +0800

    feat: support static build

--- face.go
+++ face.go
@@ -1,7 +1,7 @@
 package face
 
 // #cgo CXXFLAGS: -std=c++1z -Wall -O3 -DNDEBUG -march=native
-// #cgo LDFLAGS: -ldlib -lblas -lcblas -llapack -ljpeg
+// #cgo LDFLAGS: -ldlib -ljpeg -llapack -lf77blas -lcblas -latlas -lgfortran -lm -lquadmath
 // #include <stdlib.h>
 // #include <stdint.h>
 // #include "facerec.h"
